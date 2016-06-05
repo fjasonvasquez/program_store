92fb0522690da3c92926c55fca1b66b75b3f26f0
fjasonvasquez <fjvasquez@aol.com>
2016-06-05 01:27:46 -0700

    Implement Author update failure

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -33,6 +33,9 @@ class AuthorsController < ApplicationController
 		if @author.update(author_params)
 			flash[:success] = "Author has been updated"
 			redirect_to @author
+		else
+			flash[:danger] = "Author has not been updated"
+			render :edit
 		end
 	end
 
