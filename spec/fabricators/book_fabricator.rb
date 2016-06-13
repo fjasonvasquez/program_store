74fc351326f943f3be50682edcf1148ce06b97f3
fjasonvasquez <fjvasquez@aol.com>
2016-06-13 01:26:13 -0700

    Add Book model

---
+++ spec/fabricators/book_fabricator.rb
@@ -0,0 +1,9 @@
+Fabricator(:book) do
+  title        "MyString"
+  isbn         "MyString"
+  description  "MyText"
+  published_at "2016-06-13"
+  publisher    nil
+  page_count   1
+  price        "9.99"
+end
