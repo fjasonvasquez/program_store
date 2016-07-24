197948e17d274f399a1e8fbff3e9ca02c1f181e1
fjasonvasquez <fjvasquez@aol.com>
2016-07-24 01:41:05 -0700

    fixing broken specs

--- app/controllers/admin/publishers_controller.rb
+++ app/controllers/admin/publishers_controller.rb
@@ -18,11 +18,36 @@ class Admin::PublishersController < ApplicationController
 		@publisher = Publisher.new(publisher_params)
 		if @publisher.save
 			flash[:success] = 'Publisher has been created.'
-			redirect_to @publisher
+			redirect_to [:admin, @publisher]
 		else
 			flash[:danger] = 'Publisher has not been created.'
 			render :new
 		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @publisher.update(publisher_params)
+			flash[:success] = 'Publisher has been updated.'
+			redirect_to [:admin, @publisher]
+		else
+			flash[:danger] = 'Publisher has not been updated.'
+			render :edit
+		end
+	end
+
+	def destroy
+		if @publisher.update(publisher_params)
+			flash[:success] = 'Publisher has been deleted.'
+			redirect_to admin_publishers_path
+		end
+	end
+
+	private
+	def publisher_params
+		params.require(:publisher).permit(:name)
 	end
 		
 end
