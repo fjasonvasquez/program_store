ac5542097af00eb8fcb9afc0eb1566d8795db5ea
fjasonvasquez <fjvasquez@aol.com>
2016-08-10 02:04:35 -0700

    Implement cart show view template

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -5,7 +5,9 @@ class CartItemsController < ApplicationController
 		@cart_item = @cart.add_book_to_items(params[:book_id])
 		if @cart_item
 			redirect_to @cart_item.cart
+		else
+			redirect_to catalogs_path
 		end
 	end
-	
+
 end
