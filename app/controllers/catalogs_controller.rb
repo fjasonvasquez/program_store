6244974afb9591f1953a4d8bf95763cc0a6126e7
fjasonvasquez <fjvasquez@aol.com>
2016-08-14 01:14:28 -0700

    Create cart with AJAX

--- app/controllers/catalogs_controller.rb
+++ app/controllers/catalogs_controller.rb
@@ -1,5 +1,5 @@
 class CatalogsController < ApplicationController
-
+	before_action :set_cart_if_session, except: [:search]
 	def index
 		@books = Book.all
 	end
@@ -10,5 +10,10 @@ class CatalogsController < ApplicationController
 
 	def search
 		@books = Book.search_by_title(params[:search_word])
+	end
+
+	private
+	def set_cart_if_session
+		set_cart if session[:cart_id]
 	end
 end
