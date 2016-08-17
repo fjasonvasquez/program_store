09e260f167bc43db91a8630a123dde5da74cbc0b
fjasonvasquez <fjvasquez@aol.com>
2016-08-16 23:50:30 -0700

    Add address

---
+++ spec/fabricators/address_fabricator.rb
@@ -0,0 +1,7 @@
+Fabricator(:address) do
+  address_line1 { Faker::Address.street_address }
+  address_line2 { Faker::Address.street_address }
+  city { Faker::Address.city }
+  zipcode { Faker::Address.zipcode }
+  user          nil
+end
