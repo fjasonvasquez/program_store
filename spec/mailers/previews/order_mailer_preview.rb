384fe155d654e09c3ad547421bae1f35bcb43830
fjasonvasquez <fjvasquez@aol.com>
2016-08-25 23:56:26 -0700

    Setup email

--- spec/mailers/previews/order_mailer_preview.rb
+++ spec/mailers/previews/order_mailer_preview.rb
@@ -1,4 +1,8 @@
 # Preview all emails at http://localhost:3000/rails/mailers/order_mailer
 class OrderMailerPreview < ActionMailer::Preview
-
+	def  notify_on_successful_order
+		customer = Customer.localhost
+		order = Order.localhost
+		OrderMailer.notify_on_successful_order(customer, order)
+	end
 end
