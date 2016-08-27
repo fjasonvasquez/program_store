4fb1a2b64ff7b7aea09625bb51389582d8e59d54
fjasonvasquez <fjvasquez@aol.com>
2016-08-27 00:33:54 -0700

    Implement orders controller create action

--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,6 @@ Rails.application.routes.draw do
 
   resources :cart, only: [:show, :destroy]
   resources :cart_items, only: [:create]
-  resources :orders, only: [:new]
+  resources :orders, only: [:new, :create]
   
 end
