66badcf15cfaa945fffbee1d6b1cca16ec4b7e81
fjasonvasquez <fjvasquez@aol.com>
2016-09-05 00:03:16 -0700

    Add random token field to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,6 +22,10 @@ class User < ActiveRecord::Base
 		user && user.authenticate(password)
 	end
 
+	def to_param
+		token
+	end
+
 protected
 	def generate_token
 		self.token = SecureRandom.urlsafe_base64
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160821071801) do
+ActiveRecord::Schema.define(version: 20160905065425) do
 
   create_table "addresses", force: :cascade do |t|
     t.string   "address_line1"
@@ -110,6 +110,7 @@ ActiveRecord::Schema.define(version: 20160821071801) do
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
     t.boolean  "admin",           default: false
+    t.string   "token"
   end
 
 end
