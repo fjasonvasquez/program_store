1187dfb38116dca87c0e24bda53aa590475a6a54
fjasonvasquez <fjvasquez@aol.com>
2016-09-04 23:54:47 -0700

    fixed migration bugs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,20 +13,11 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  config.action_mailer.deliver_method = :smtp
-  config.action_mailer.smtp_settings = {
-      address:              'smtp.gmail.com',
-      port:                 587,
-      domain:               'example.com',
-      user_name:            ENV['GMAIL_USERNAME'],
-      password:             ENV['GMAIL_PASSWORD'],
-      authentication:       'plain'
-      enable_starttls_auto: true }
-
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => "localhost:3000"}
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => "localhost:3000" }
+  config.action_mailer.delivery_method = :letter_opener
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -39,14 +30,10 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
-  # Asset digests allow you to set far-future HTTP expiration dates on all assets,
-  # yet still be able to expire them through the digest params.
-  config.assets.digest = true
-
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
-  config.assets.raise_runtime_errors = true
+  config.assets.raise_runtime_errors = false
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
