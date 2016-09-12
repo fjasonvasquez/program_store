45ff69c473bd12ec99bfe6db5d3d87b1285d8812
fjasonvasquez <fjvasquez@aol.com>
2016-09-11 23:45:52 -0700

    Implement password reset feature test

---
+++ spec/features/password_reset_feature_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.feature "Resetting Users' Password" do 
+	let(:john) { Fabricate(:user, email: 'john@example.com' ) }
+
+	scenario 'with valid email address' do 
+		visit root_path
+
+		click_link 'Sign in'
+		click_link 'Forgotten Password?'
+
+		fill_in 'Email Address', with: john.email
+		click_link 'Reset Password'
+
+		expect(page).to have_content('An email with instructions')
+	end
+
+	scenario 'with invalid email address' do 
+		visit root_path
+
+		click_link 'Sign in'
+		click_link 'Forgotten Password?'
+
+		fill_in 'Email Address', with: 'james@example.com'
+		click_link 'Reset Password'
+
+		expect(page).to have_content('Email invalid')
+	end
